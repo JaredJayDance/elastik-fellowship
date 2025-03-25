0dbec4589e32539b96df5d92492e8fc3441292d2
Jay Dance <jaredjaydance@gmail.com>
2025-03-26 09:52:32 +1100

    Frontend student creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import type { Schema } from "../amplify/data/resource";
+import type { Schema, jaySchema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
+const jayClient = generateClient<jaySchema>();
 
 function App() {
   const { user, signOut } = useAuthenticator();
@@ -23,6 +24,30 @@ function App() {
     client.models.Todo.delete({ id })
   }
 
+
+  const [students, showStudents] = useState<Array<jaySchema["StudentList"]["type"]>>([]);
+
+  useEffect(() => {
+    jayClient.models.StudentList.observeQuery().subscribe({
+      next: (data) => showStudents([...data.items]),
+    });
+  }, []);
+
+  //Create a student entry in the DB or will this make a different student with each create() call??
+  function createStudent() {
+    jayClient.models.StudentList.create({email: window.prompt("Add email")});
+    jayClient.models.StudentList.create({firstName: window.prompt("First Name")});
+    jayClient.models.StudentList.create({lastName: window.prompt("Last Name")});
+    jayClient.models.StudentList.create({DOB: window.prompt("Students DOB")});
+    jayClient.models.StudentList.create({schoolName: window.prompt("School")});
+    jayClient.models.StudentList.create({coordinatorName: window.prompt("Coordinator")});
+    jayClient.models.StudentList.create({teacherName: window.prompt("Teacher")});
+  }
+
+  function deleteStudent(id: string) {
+    jayClient.models.StudentList.delete({ id })
+  }
+
   return (
     <main>
       <h1>{user?.signInDetails?.loginId}'s ToDos</h1>
@@ -35,6 +60,14 @@ function App() {
         ))}
       </ul>
       <button onClick={signOut}>Sign out</button>
+      <button onClick={createStudent}>+ newStudent</button>
+      <ul>
+        {students.map((student) => (
+          <li 
+          onClick={() => deleteStudent(student.id)}
+          key={student.id}>{student.firstName}</li>
+        ))}
+      </ul>
     </main>
   );
 }
