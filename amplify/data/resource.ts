bcae8d61c3013491e26a12828f833cc17a8445f0
Jay Dance <jaredjaydance@gmail.com>
2025-03-28 15:43:30 +1100

    Does this work?

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,6 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+//import { handler } from "../functions/api-function/handler";
+import { myApiFunction } from "../functions/api-function/resource";
 
 const schema = a.schema({
   StudentList: a
@@ -13,6 +15,11 @@ const schema = a.schema({
 
     }).authorization(allow => [allow.authenticated()]),
 
+  queryStudents: a
+    .query()
+    .returns(a.json())
+    .authorization(allow => [allow.guest()]) //change to authenticated later
+    .handler(a.handler.function(myApiFunction)),
 });
 
 export type Schema = ClientSchema<typeof schema>;
