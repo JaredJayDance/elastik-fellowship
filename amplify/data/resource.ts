dfcc6e919f0cb6b521aac57081d7a2f9252e1ada
Jay Dance <jaredjaydance@gmail.com>
2025-03-28 14:49:39 +1100

    Updated amplifyoutputs to see if that fixes

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,9 +1,6 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 //import { handler } from "../functions/api-function/handler";
-// import { myApiFunction } from "../functions/api-function/resource";
-
-import * as myApiFunction from "../functions/api-function/resource"
-console.log("function imported",myApiFunction)
+import { myApiFunction } from "../functions/api-function/resource";
 
 const schema = a.schema({
   StudentList: a
@@ -18,11 +15,11 @@ const schema = a.schema({
 
     }).authorization(allow => [allow.authenticated()]),
 
-  /*queryStudents: a
+  queryStudents: a
     .query()
     .returns(a.json())
     .authorization(allow => [allow.guest()]) //change to authenticated later
-    .handler(a.handler.function(myApiFunction)),*/
+    .handler(a.handler.function(myApiFunction)),
 });
 
 export type Schema = ClientSchema<typeof schema>;
--- amplify/functions/api-function/resource.ts
+++ amplify/functions/api-function/resource.ts
@@ -3,5 +3,3 @@ import { defineFunction } from "@aws-amplify/backend";
 export const myApiFunction = defineFunction({
   name: "myApiFunction",
 });
-
-console.log("my function is loaded.");
