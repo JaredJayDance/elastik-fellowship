c42ee054f3180d88b662e4b006fdd11494956692
Jay Dance <jaredjaydance@gmail.com>
2025-03-30 14:39:35 +1100

    Quick fix

--- src/table.tsx
+++ src/table.tsx
@@ -13,7 +13,7 @@ const MyTable = () => {
 
   const fetchStudents = async () => {
     const { data: fetchedStudents } = await jayClient.models.StudentList.list();
-    console.log("Pre-mapping: " +fetchedStudents);
+    console.log("Pre-mapping: " + fetchedStudents);
     interface Student {
       email: string;
       firstName: string;
@@ -24,7 +24,7 @@ const MyTable = () => {
       teacherName: string;
     }
 
-    const students = fetchedStudents.map(student => ({
+    const students: Student[] = fetchedStudents.map(student => ({
       email: student?.email ?? "empty",
       firstName: student?.firstName ?? "empty",
       lastName: student?.lastName ?? "empty",
@@ -34,13 +34,24 @@ const MyTable = () => {
       teacherName: student?.teacherName ?? "empty"
     }));
 
-    console.log("After mapping: " + students);
-
-    return students as Student[];
+    return students;
   };
 
-  const [rowData, setRowData]: any[] = useState(fetchStudents);
-  console.log(setRowData);
+  const [rowData, setRowData]: any[] = useState(fetchStudents());
+
+  /*
+  const myArray = fetchStudents();
+  for (let i = 0, len = myArray.length; i < len; i++) {
+    console.log(myArray[i].email);
+    console.log(myArray[i].firstName);
+    console.log(myArray[i].lastName);
+    console.log(myArray[i].schoolName);
+    console.log(myArray[i].coordinatorName);
+    console.log(myArray[i].teacherName);
+  }
+  */
+    console.log("After mapping: " + fetchStudents);
+    console.log("Row data print: " + setRowData);
 
 const [colsDef, setColsDef]: any[] = useState([
   { field: "email" },
@@ -51,7 +62,7 @@ const [colsDef, setColsDef]: any[] = useState([
   { field: "coordinatorName" },
   { field: "teacherName" },
 ]);  
-console.log(setColsDef);
+console.log("ColsDef print: " + setColsDef);
   
   const defaultColDef = {
     flex: 1,
@@ -64,7 +75,7 @@ console.log(setColsDef);
         columnDefs={colsDef}
         defaultColDef={defaultColDef}
         pagination={true}
-        paginationPageSize={10}
+        paginationPageSize={20}
       />
     </div>
   );
